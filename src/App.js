1670393b57852c1bf2469472f9f8f84a5d2ab3c7
Aditya Tiwari <adiaditiwari@gmail.com>
2020-03-23 11:04:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Cardlist } from './components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      strings: []
+    }
+  }
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ strings: users }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Cardlist strings={this.state.strings} />
+      </div>
+    )
+  }
+
 }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './card-list.styles.css';
+import { Card } from './../card/card.component';
+
+export const Cardlist = props => (
+    <div className='card-list'>
+        {
+            props.strings.map(string => (
+                <Card key={string.id} string={string} />
+            ))
+        }
+    </div>
+)
